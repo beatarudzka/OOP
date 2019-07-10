ef3b485b2f112b054a33522fd638e971960f5ba5
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-10 14:27:06 +0200

    new  + createElement

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-///new tworzy nowy obiekt na podstawie funkcji specjalnego rodzaju (ma specjalny proto), przypisuje nowy proto, wywoluje funkcje w konteście nowego obiektu, zwraca nowy obiekt
-//
-function Button(text) {
+function Button(text) { //
     this.label = text
+
+    this.init = function() {
+        const button = document.createElement('button')
+        button.innerText = this.label
+        document.body.appendChild(button)
+    }
+    this.init
 }
 
 const btn1 = new Button('Click me')
+const btn2 = new Button('Click')
