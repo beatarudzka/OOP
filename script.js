08c965c48231e3414508b95f34e17ba57f2298cb
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-10 10:25:37 +0200

    this in arrow function

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-//dziedzieczenie prototypowe
-// dziedziczenie klasowe
-// this zawsze wskazuje na obiekt i na kontekt wywołany czyli na miejsce w którym zostala wywołana funkcja
-// jesli bedziemy uzywac modułów - wewnątrz modułow this wskazuje na indefiend
-// this nie można go nadpisać - silnik decyduje na co wskauje ten wskażnik
-//
+//funkcja strzałkowa zapamietuje this na miejsca deklaracji a deklaracja może być w innej funkcji
+// 
 
-
-function sayHello() {
+const sayHello = () => {
     console.log('Hello' + this.name)
 }
-
-const me = {
-    name: "Kundzia"
-}
-
-// this wskazuje na to co stoi przed kropką
-// to się tyczy tylko do zwykłych funkcji
