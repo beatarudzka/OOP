ed1480fd6d47bdc162252b4178cfc4a4cc80800e
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-11 11:38:04 +0200

    button +addEvenListener

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-function Button(text) {
-    this.label = text
+function Button(label, onClick) {
+    this.label = label
+    this.onClick = onClick
     this.element = null
 }
 
 Button.prototype.init = function() {
     this.element = document.createElement('button')
+
     this.element.innerText = this.label
-    document.body.appendChild(this.element)
+
     this.element.addEventListener(
         'click',
-        function
+        this.onClick
     )
 
+    document.body.appendChild(this.element)
 }
+
 Button.prototype.setLabel = function(newLabel) {
     this.label = newLabel
+
     if (this.element) {
         this.element.innerText = this.label
     }
 }
 
-const btn1 = new Button('Click me', () => alert('Clicked'))
-btn1.init()
+Button.prototype.setOnClick = function(newOnClick) {
+    const oldOnClick = this.onClick
+    this.onClick = newOnClick
+
+    if (this.element) {
+        this.element.addEventListener(
+            'click',
+            this.onClick
+        )
+        this.element.removeEventListener(
+            'click',
+            oldOnClick
+        )
+    }
+}
