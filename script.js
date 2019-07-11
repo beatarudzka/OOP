441dfb010b2a41bf2d6eadfcce78d2a6e118f3ab
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-11 10:45:43 +0200

    addEventListner and creating button

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-function Button(text) { //
+function Button(text) {
     this.label = text
-
-    this.init = function() {
-        const button = document.createElement('button')
-        button.innerText = this.label
-        document.body.appendChild(button)
-    }
-    this.init
+    this.element = null
 }
 
-const btn1 = new Button('Click me')
-const btn2 = new Button('Click')
+Button.prototype.init = function() {
+    this.element = document.createElement('button')
+    this.element.innerText = this.label
+    document.body.appendChild(this.element)
+    this.element.addEventListener(
+        'click',
+        function
+    )
+
+}
+Button.prototype.setLabel = function(newLabel) {
+    this.label = newLabel
+    if (this.element) {
+        this.element.innerText = this.label
+    }
+}
+
+const btn1 = new Button('Click me', () => alert('Clicked'))
+btn1.init()
