023075b372604e9a335f75e211ec1e90445cba7f
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-12 12:36:36 +0200

    class and rectangular

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-function Rectangle(a, b, backgroundColor) {
-    this.a = a
-    this.b = b
-    this.backgroundColor = backgroundColor
-    this.element = null
+class Rectangle {
+    constructor(a, b, backgroundColor) {
+        this.a = a
+        this.b = b
+        this.backgroundColor = backgroundColor
+        this.element = null
+    }
+
+    render() {
+        this.element = this.element || document.createElement('div')
+
+        this.element.style.width = this.a + 'px'
+        this.element.style.height = this.b + 'px'
+        this.element.style.backgroundColor = this.backgroundColor
+
+        return this
+    }
+
+    append(container = document.body) {
+        container.appendChild(this.element)
+
+        return this
+    }
 }
-
-Rectangle.prototype.render = function() {
-    this.element = document.createElement('div')
-
-    this.element.style.width = this.a + 'px'
-    this.element.style.height = this.b + 'px'
-    this.element.style.backgroundColor = this.backgroundColor
-
-    return this
-}
-
-Rectangle.prototype.append = function(container = document.body) {
-    this.element && container.appendChild(this.element)
-
-    return this
-}
-
-const rectangle1 = new Rectangle(100, 50, 'red')
-const rectangle2 = new Rectangle(50, 100, 'green')
-
-rectangle1
-    .render()
-    .append()
