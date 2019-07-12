b61b31c957e2c2da4b52eeeca76f59fad96b53c8
Beata Rudzka <beatarudzka89@gmail.com>
2019-07-12 15:23:22 +0200

    moving  square

--- script.js
+++ script.js
@@ -47,13 +47,76 @@ class Game {
         }
         this.square = (new Square(20, 'red')).render()
     }
+    init() {
+        this.square = new Square(20, 'red')
+        this.square.render()
+        this.square.element.style.position = 'absolute'
+
+        this.render()
+        this.startListeningToControls()
+        this.append()
+        this.startGameTick()
+    }
+
+
+    move(deltaX, deltaY) {
+        this.position = {
+            x: this.position.x + deltaX,
+            y: this.position.y + deltaY
+
+        }
+    }
+
+    startGameTick() {
+        setInterval(
+            () => {
+                (event) => {
+                    if (LastKeyDown === 'ArrowRight') {
+                        this.move(10, 0)
+                    }
+                    if (LastKeyDown === 'ArrowLeft') {
+                        this.move(-10, 0)
+                    }
+                    if (LastKeyDown === 'ArrowUp') {
+                        this.move(0, -10)
+                    }
+                    if (LastKeyDown === 'ArrowDown') {
+                        this.move(0, 10)
+                    }
+                },
+                250
+            }
+        )
+    }
+
+    startListeningToControls() {
+        window.addEventListener(
+            'keydown',
+            (event) => {
+                this.LastKeyDown = event.key
+
+            }
+
+
+        )
+    }
+    render() {
+        this.square.element.style.position = 'absolute'
+        this.square.element.style.left = this.position.x + 'px'
+        this.square.element.style.top = this.position.y + 'px'
+        return this
+    }
+
 
     append(container = document.body) {
         container.appendChild(this.square.element)
 
         return this
     }
+
 }
+
 
 const game1 = new Game()
 game1.append()
+game1.render()
